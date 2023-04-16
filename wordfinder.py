1c9c645afcc3951d10b59842ecc22525a147fd19
LindseyIpson <lindseyipson@gmail.com>
2023-04-15 20:09:30 -0400

    update __repr__ and __str__ methods

--- wordfinder.py
+++ wordfinder.py
@@ -19,12 +19,16 @@ class WordFinder:
 
     def __init__ (self, path_to_file):
         """Reads dictionary and reports # items read"""
+        self.path_to_file = path_to_file
         dict_file = open(path_to_file)
         self.words = self.parse(dict_file)
         print(f"{len(self.words)} words read")
 
     def __repr__ (self):
-        return f"Words in this finder are: {self.words}"
+        return f"<WordFinder path_to_file={self.path_to_file}>"
+
+    def __str__ (self):
+        return f"Finding from: {self.path_to_file}, these words: {self.words}"
 
     def parse (self, dict_file):
         """Parses dict_file for list of words"""
@@ -33,7 +37,6 @@ class WordFinder:
     def random (self):
         """Returns random word"""
         return random.choice(self.words)
-
 
 class SpecialWordFinder(WordFinder):
     """Specialized WordFinder that excludes blank lines/comments.
@@ -50,6 +53,9 @@ class SpecialWordFinder(WordFinder):
     >>> swf.random() in ["kale", "parsnips", "apple", "mango"]
     True
     """
+
+    def __repr__ (self):
+        return f"<SpecialWordFinder path_to_file={self.path_to_file}>"
 
     def parse (self, dict_file):
         return [word.strip() for word in dict_file if word.strip() and not word.startswith('#')]
